ca82c66b3e9f0e2f5bae7ab36e773b1b3e96d69a
Tom Hutchings <goerofmuns@gmail.com>
2016-01-03 19:55:34 +0100

    really created constants file

---
+++ src/constants.hpp
@@ -0,0 +1,14 @@
+#ifndef __CONSTANTS_H__
+#define __CONSTANTS_H__
+
+const float GLOBAL_SCALE = 2;
+
+//Player things
+const float X_SPEED = 0.3;
+const float X_MIN   = -400;
+const float X_MAX   = 140;
+const float Y_SPEED = 0.3;
+const float Y_MAX = 20;
+const float Y_MIN = -20;
+
+#endif 
