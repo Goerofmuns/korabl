5bfddd97e9c7627d51638d8f488c468ca99538f0
Tom Hutchings <goerofmuns@gmail.com>
2016-12-19 15:46:24 +0100

    more maths

--- src/space_math.hpp
+++ src/space_math.hpp
@@ -1,12 +1,13 @@
 #include<cmath>
 
-float lerp(float value, float start, float end)
-{
-    return (end - start) * value;
-}
 
-namespace mth
+namespace math
 {
+    float lerp(float value, float start, float end)
+    {
+        return (end - start) * value;
+    }
+
     float norm(float n)
     {
         if(n > 0)
@@ -15,7 +16,7 @@ namespace mth
             return -1;
     }
 
-    namespace v2
+    namespace Vector
     {
         class Vector2
         {
@@ -30,6 +31,12 @@ namespace mth
             double length()
             {
                 sqrtf(this->X * this->X + this->Y * this->Y);
+            }
+
+            void translate(Vector2 v2)
+            {
+                this->X += v2.X;
+                this->Y += v2.Y;
             }
 
             void scale(double s)
@@ -50,10 +57,7 @@ namespace mth
                 this->Y = this->Y * v2.Y;
             }
         }
-    }
 
-	namespace v3
-	{
 		class Vector3
 		{
 		public:
@@ -64,29 +68,28 @@ namespace mth
 				Y = _y;
 				Z = _z;
 			}
+
+            double vlength(Vector3 v)
+            {
+                return sqrtf(v.X * v.X + v.Y * v.Y + v.Z * v.Z);
+            }
+
+            Vector3 vscale(Vector3 v, float s)
+            {
+                return (v.X * s) + (v.Y * s) + (v.Z * s);
+            }
+
+            Vector3 vnormalize(Vector3 v)
+            {
+                double l = vlength(v);
+                return vscale(v, 1.0 / l);
+            }
+
+            Vector3 vdot(Vector3 v1, Vector3 v2)
+            {
+                Vector3 v_ret(v1.X * v2.X, v1.Y * v2.Y, v1.Z * v2.Z);
+                return v_ret;
+            }
 		};
-
-		double vlength(Vector3 v)
-		{
-			return sqrtf(v.X * v.X + v.Y * v.Y + v.Z * v.Z);
-		}
-
-		Vector3 vscale(Vector3 v, float s)
-		{
-			return (v.X * s) + (v.Y * s) + (v.Z * s);
-		}
-
-		Vector3 vnormalize(Vector3 v)
-		{
-			double l = vlength(v);
-			return vscale(v, 1.0 / l);
-		}
-
-		Vector3 vdot(Vector3 v1, Vector3 v2)
-		{
-			Vector3 v_ret(v1.X * v2.X, v1.Y * v2.Y, v1.Z * v2.Z);
-			return v_ret;
-		}
 	}
-    
 }
